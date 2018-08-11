d161610c5912194f9d00191ebb98e61d64347873
Gabriel Nikol <GabrielNikol@web.de>
2018-08-11 21:42:04 +0200

    "fixed" test_ueq for jenkins refactored example_test.cpp

--- hello_world_tests/basic_tests/example_test.cpp
+++ hello_world_tests/basic_tests/example_test.cpp
@@ -1,11 +1,10 @@
 #include "gtest/gtest.h"
 
-TEST(sample_test_case, test_eq)
-{
+TEST(sample_test_case, test_eq) {
     EXPECT_EQ(1, 1);
 }
 
-TEST(sample_test_case, test_ueq)
-{
-    EXPECT_EQ(0, 1);
+TEST(sample_test_case, test_ueq) {
+    //EXPECT_EQ(0, 1);
+    EXPECT_NE(0, 1);
 }
